551cc2801ae6545645941559b982b9f5349d86a2
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-05-09 23:07:14 +0530

    Frontend script

---
+++ frontend.py
@@ -0,0 +1,34 @@
+from sqlite3 import Row
+from tkinter import *
+
+window=Tk()
+
+l1=Label(window,text="Title")
+l1.grid(row=0,column=0)
+
+l2=Label(window,text="Author")
+l2.grid(row=0,column=2)
+
+l3=Label(window,text="Year")
+l3.grid(row=1,column=0)
+
+l4=Label(window,text="ISBN")
+l4.grid(row=1,column=2)
+
+title_text=StringVar()
+e1=Entry(window,textvariable=title_text)
+e1.grid(row=0,column=1)
+
+author_text=StringVar()
+e2=Entry(window,textvariable=author_text)
+e2.grid(row=0,column=3)
+
+year_text=StringVar()
+e3=Entry(window,textvariable=year_text)
+e3.grid(row=1,column=1)
+
+isbn_text=StringVar()
+e4=Entry(window,textvariable=isbn_text)
+e4.grid(row=1,column=3)
+
+window.mainloop()

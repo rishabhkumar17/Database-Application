e42b2d2a74708e1deb3bb92b84bc7d4016746a0d
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-05-09 23:20:05 +0530

    Interface added

--- frontend.py
+++ frontend.py
@@ -31,4 +31,31 @@ isbn_text=StringVar()
 e4=Entry(window,textvariable=isbn_text)
 e4.grid(row=1,column=3)
 
+list1=Listbox(window, height=6,width=35)
+list1.grid(row=2,column=0,rowspan=6,columnspan=2)
+
+sb1=Scrollbar(window)
+sb1.grid(row=2,column=2,rowspan=6)
+
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
+b1=Button(window,text="View All",width=12)
+b1.grid(row=2,column=3)
+
+b2=Button(window,text="Search Entry",width=12)
+b2.grid(row=3,column=3)
+
+b3=Button(window,text="Add Entry",width=12)
+b3.grid(row=4,column=3)
+
+b4=Button(window,text="Update Selected",width=12)
+b4.grid(row=5,column=3)
+
+b5=Button(window,text="Delete Selected",width=12)
+b5.grid(row=6,column=3)
+
+b6=Button(window,text="Close",width=12)
+b6.grid(row=7,column=3)
+
 window.mainloop()

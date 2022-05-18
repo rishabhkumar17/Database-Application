c710a5171ee926df5e7d055cd8c2442993180497
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-05-18 21:24:50 +0530

    created an entry in database

---
+++ backend.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+def connect():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    conn.commit()
+    conn.close()
+
+def insert(title, author, year, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+connect()
+insert("Wings of Fire","A.P.J Abdul Kalam", 1999, 12345678)
+print(view())

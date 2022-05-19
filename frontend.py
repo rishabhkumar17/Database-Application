1aae758b62a45b2c6cb474ad3e645f7fdd44b987
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-05-19 22:47:18 +0530

    Connecting frontend to backend

--- frontend.py
+++ frontend.py
@@ -1,6 +1,17 @@
 from sqlite3 import Row
 from tkinter import *
 import backend
+
+def view_command():
+    list1.delete(0,END)
+    for row in backend.view():
+        list1.insert(END,row)
+
+def search_command():
+    list1.delete(0,END)
+    for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+        list1.insert(END,row)
+
 window=Tk()
 
 l1=Label(window,text="Title")
@@ -40,10 +51,10 @@ sb1.grid(row=2,column=2,rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1=Button(window,text="View All",width=12)
+b1=Button(window,text="View All",width=12,command=view_command)
 b1.grid(row=2,column=3)
 
-b2=Button(window,text="Search Entry",width=12)
+b2=Button(window,text="Search Entry",width=12,command=search_command)
 b2.grid(row=3,column=3)
 
 b3=Button(window,text="Add Entry",width=12)

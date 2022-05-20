9d788c5ee8627fe12f9c9c497a9732461e087fb2
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-05-20 20:37:32 +0530

    delete command

--- frontend.py
+++ frontend.py
@@ -3,9 +3,9 @@ from tkinter import *
 import backend
 
 def get_selected_row(event):
+    global selected_tuple
     index=list1.curselection()[0]
     selected_tuple=list1.get(index)
-    print(selected_tuple)
 
 def view_command():
     list1.delete(0,END)
@@ -21,6 +21,9 @@ def add_command():
     backend.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
     list1.delete(0,END)
     list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
+
+def delete_command():
+    backend.delete(selected_tuple[0])
 
 window=Tk()
 
@@ -75,7 +78,7 @@ b3.grid(row=4,column=3)
 b4=Button(window,text="Update Selected",width=12)
 b4.grid(row=5,column=3)
 
-b5=Button(window,text="Delete Selected",width=12)
+b5=Button(window,text="Delete Selected",width=12,command=delete_command)
 b5.grid(row=6,column=3)
 
 b6=Button(window,text="Close",width=12)

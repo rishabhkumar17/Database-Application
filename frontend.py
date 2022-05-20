a57b95b3571265e0ee66b2768bf7fd98badde532
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-05-20 20:42:31 +0530

    select and view

--- frontend.py
+++ frontend.py
@@ -6,6 +6,14 @@ def get_selected_row(event):
     global selected_tuple
     index=list1.curselection()[0]
     selected_tuple=list1.get(index)
+    e1.delete(0,END)
+    e1.insert(END,selected_tuple[1])
+    e2.delete(0,END)
+    e2.insert(END,selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(END,selected_tuple[4])
 
 def view_command():
     list1.delete(0,END)
